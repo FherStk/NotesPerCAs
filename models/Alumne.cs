10b0fda0661d6cd924cc746b274375ed39a44b2f
FherStk <porrino.fernando@gmail.com>
2024-03-14 15:38:40 +0100

    Added CAs to recover

--- models/Alumne.cs
+++ models/Alumne.cs
@@ -14,12 +14,25 @@ public class Alumne{
     public String toString(){
         var info = $"{this.Cognoms}, {this.Nom} ({this.Email}):\n";
         foreach(var mp in MPs){  
-            info += $"   {(mp.Nom ?? "").Replace(" (Real)", "")}:\n";
+            info += $"   {clean(mp.Nom)}:\n";
 
             foreach(var uf in mp.UFs){
                 var nota = uf.Nota;
-                if(nota != null) info += $"      {uf.Nom}:\n         Nota Real: {nota.Real}\n         Nota mitja: {nota.Mitja}\n";
-                else info += $"      {uf.Nom}:\n         Encara no s'ha cursat.\n";
+                if(nota != null) info += $"      {clean(uf.Nom)}:\n         Nota Real: {nota.Real}\n         Nota mitja: {nota.Mitja}\n";
+                else info += $"      {clean(uf.Nom)}:\n         Encara no s'ha cursat.\n";
+
+                if(nota != null && nota.Mitja != nota.Real){
+                    //Cal recuperar quelcom
+
+                    info += $"         Cal recuperar els següents CAs:\n";
+                    foreach(var ra in uf.RAs){
+                        foreach(var ca in ra.CAs){
+                            if(ca.Nota != null && ca.Nota.Real < 4){
+                                info += $"            {clean(ra.Nom)} CA {clean(ca.Nom)}\n";
+                            }
+                        }
+                    }
+                }
             }
 
             info += "\n";
@@ -27,6 +40,9 @@ public class Alumne{
         
 
         return info;
+    }
 
+    private String clean(String? text){
+        return (text ?? "").Replace(" (Real)", "");
     }
 }
